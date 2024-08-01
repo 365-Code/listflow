f1d6677ad782b055e195556ce71a21870b450d68
365-Code <sohailahmedbhati786@gmail.com>
2024-08-01 12:49:37 +0530

    api route changed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = 3000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://listflow.vercel.app",
   credentials: true
 }));
 
